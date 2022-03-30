2d69d3f889dca43212eab262a18837985d25bbc2
Luiz <luizmouratavares@gmail.com>
2022-03-30 08:54:27 -0300

    Aprendizado em Java !

---
+++ src/Pessoa.java
@@ -0,0 +1,7 @@
+public class Pessoa {
+
+    private String nome;
+    private String endereco;
+    private String telefone;
+    private String cpf;
+}
---
+++ src/Teste.java
@@ -0,0 +1,9 @@
+public class Teste {
+
+    public static void main(String[] args) {
+
+        Aluno aluno = new Aluno();
+
+        Professor professor = new Professor();
+    }
+}
